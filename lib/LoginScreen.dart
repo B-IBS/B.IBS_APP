e9c4be9dd27856c4757164b1f947c40385d7612c
Henrixounez <30901439+Henrixounez@users.noreply.github.com>
2019-12-09 15:12:17 +0100

    [ADD]: Login screen better visuals (#13)

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -6,9 +6,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(
-        child: LoginForm2(),
-      ),
+      body: LoginForm2()
     );
   }
 }
@@ -27,70 +25,68 @@ class LoginForm2State extends State<LoginForm2> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(left: 75, right: 75, top: 50),
-              child: Image.asset(
-                'assets/images/LOGO_BIBS_150x150mm-02_WHITE.png',
-              ),
+    return SingleChildScrollView(
+      padding: EdgeInsets.only(bottom: 50),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 75, right: 75, top: 50),
+            child: Image.asset(
+              'assets/images/LOGO_BIBS_150x150mm-02_WHITE.png',
             ),
-            Form(
-              key: _formKey,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                margin: EdgeInsets.symmetric(horizontal: 50),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20)
-                ),
-                child: Column(
-                  children: <Widget>[
-                    formField(text: "Email", icon: Icon(Icons.person, color: Colors.grey[600]), controller: _loginController, obscure: false),
-                    formField(text: "Password", icon: Icon(Icons.lock, color: Colors.grey[600]), controller: _passwordController, obscure: true),
-                    passwordConfirmField(text: "Confirm Password", icon: Icon(Icons.lock, color: Colors.grey[600]), controller: _passwordConfirmController, obscure: true),
-                    Container(
-                      margin: EdgeInsets.only(top: 20),
-                      child: RaisedButton(
-                        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 12),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        onPressed: () {
-                          print(_loginController.text);
-                          print(_passwordController.text);
-                        },
-                        child: Text('LOGIN', style: TextStyle(fontSize: 15, letterSpacing: 1.5),),
-                        color: Colors.black,
-                        textColor: Colors.white,
+          ),
+          Form(
+            key: _formKey,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              margin: EdgeInsets.symmetric(horizontal: 50),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Column(
+                children: <Widget>[
+                  formField(text: "Email", icon: Icon(Icons.person, color: Colors.grey[600]), controller: _loginController, obscure: false),
+                  formField(text: "Password", icon: Icon(Icons.lock, color: Colors.grey[600]), controller: _passwordController, obscure: true),
+                  passwordConfirmField(text: "Confirm Password", icon: Icon(Icons.lock, color: Colors.grey[600]), controller: _passwordConfirmController, obscure: true),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: RaisedButton(
+                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
                       ),
+                      onPressed: loginToServer,
+                      child: Text(_isSignin ? 'SIGNUP' : 'LOGIN', style: TextStyle(fontSize: 15, letterSpacing: 1.5),),
+                      color: Colors.black,
+                      elevation: 7,
+                      textColor: Colors.white,
                     ),
-                    Container(
-                      margin: EdgeInsets.only(bottom: 15),
-                      child: FlatButton(
-                        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            _isSignin = !_isSignin;
-                          });
-                        },
-                        child: Text('SIGNUP', style: TextStyle(fontSize: 15, letterSpacing: 1.5),),
-                        color: Colors.transparent,
-                        textColor: Colors.black,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 15, top: 5),
+                    child: RaisedButton(
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
                       ),
+                      onPressed: () {
+                        setState(() {
+                          _isSignin = !_isSignin;
+                        });
+                      },
+                      child: Text(_isSignin ? 'LOGIN TO ACCOUNT' : 'CREATE AN ACCOUNT', style: TextStyle(fontSize: 15, letterSpacing: 1.5),),
+                      color: Colors.white,
+                      elevation: 7,
+                      textColor: Colors.black,
                     ),
-                  ],
-                ),
-              )
+                  ),
+                ],
+              ),
             )
-          ],
-        )
+          )
+        ],
       )
     );
   }
@@ -128,5 +124,10 @@ class LoginForm2State extends State<LoginForm2> {
         )
       )
     );
+  }
+
+  void loginToServer() {
+    print('Login: ' + _loginController.text);
+    print('Password: ' + _passwordController.text);
   }
 }
